48e728098acc4a9c739810a97ff936f267568379
Andriel <134160567+AndrielHenrique@users.noreply.github.com>
2024-10-13 13:02:35 -0300

    Exercicios Introdutivos  e Estrutura de Decisão

---
+++ Exercicios Basicos/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        exercicio1();
+        exercicio2();
+        exercicio3();
+        exercicio4();
+    }
+
+    static void exercicio1()
+    {
+        Console.WriteLine("1. Desenvolva uma calculadora que leia dois números do usuário (double) e em seguida imprima a soma, subtração, multiplicação e divisão do primeiro pelo segundo.");
+        double n1 = 0, n2 = 0;
+        Console.WriteLine("Calculadora Simples:\nInforme o primeiro numero: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe o segundo numero: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Primeiro valor é: " + n1 + "\nSegundo valor é: " + n2 + "\nSoma: " + (n1 + n2) + "\nSubtração: " + (n1 - n2) + "\nMultiplicação: " + (n1 * n2) + "\nDivisão: " + (n1 / n2));
+    }
+
+    static void exercicio2()
+    {
+        Console.WriteLine("2. Crie programa que leia o raio de uma circunferência e então calcule sua área e a apresente.");
+        Console.WriteLine("Informe o raio da circunferência: ");
+        double r = Convert.ToDouble(Console.ReadLine());
+        double area = (Math.PI * Math.Pow(r, 2));
+        Console.WriteLine("A área da circunferência é: " + area);
+    }
+
+    static void exercicio3()
+    {
+        Console.WriteLine("3. Leia as medidas dos catetos de um triângulo retângulo e calcule a medida da hipotenusa.");
+        Console.WriteLine("Informe a medida do cateto A do triângulo retângulo: ");
+        double catetoA = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("informe a medida do cateto B do triângulo retângulo: ");
+        double catetoB = Convert.ToDouble(Console.ReadLine());
+
+        double h = Math.Sqrt(Math.Pow(catetoA, 2) + Math.Pow(catetoB, 2));
+        Console.WriteLine("Hipotenusa do triangulo retangulo: " + h);
+    }
+
+    static void exercicio4()
+    {
+        Console.WriteLine("4. Leia a altura e o raio de uma lata de óleo e calcule seu volume.");
+        Console.WriteLine("Informe a altura da lata de óleo: ");
+        double h = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe o raio da lata de óleo: ");
+        double r = Convert.ToDouble(Console.ReadLine());
+
+        double v = (Math.PI * Math.Pow(r, 2) * h);
+        Console.WriteLine("O volume do cilindro de altura " + h + " e raio " + r + " é = " + v);
+
+    }
+}
+
---
+++ Exercicios Estruturas de Decisão/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Exercicios_Estruturas_de_Decisão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            exercicio1();
+            exercicio2();
+        }
+        static void exercicio1()
+        {
+            // 1. Desenvolver um programa que leia duas notas entre 0 e 10, calcule a média e exiba:
+            // • APROVADO – Se a nota está entre 6 e 10.
+            // • RECUPERAÇÃO – Se a nota está entre 4 e 5.9.
+            // • REPROVADO – Se a nota está abaixo de 4.
+
+            Console.WriteLine("Informe a primeira nota: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a segunda nota: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+            double media = (n1 + n2) / 2;
+            if (media > 6)
+                Console.WriteLine("Aprovado " + media);
+            else if (media >= 4 && media < 6)
+                Console.WriteLine("Recuperação " + media);
+            else
+                Console.WriteLine("Reprovado " + media);
+        }
+
+        static void exercicio2()
+        {
+            // 2. Usando a estrutura If, desenvolva um programa que leia três números inteiros. Em seguida exiba-os em ordem crescente.
+            Console.WriteLine("Informe o primeiro numero: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o segundo numero: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o terceiro numero: ");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.Write(n1 + " ");
+                if (n2 > n3)
+                    Console.Write(n2 + " " + n3);
+                else
+                    Console.Write(n3 + " " + n2);
+            }
+            else if (n2 > n3 && n2 > n1)
+            {
+                Console.Write(n2 + " ");
+                if (n3 > n1)
+                    Console.Write(n3 + " " + n1);
+                else
+                    Console.Write(n1 + " " + n3);
+            }
+            else
+            {
+                Console.Write(n3 + " ");
+                if (n1 > n2)
+                    Console.Write(n1 + " " + n2);
+                else
+                    Console.Write(n2 + " " + n1);
+            }
+        }
+
+        static void exercicio3()
+        {
+
+        }
+    }
+}
