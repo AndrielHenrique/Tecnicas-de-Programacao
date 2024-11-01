9f0303230a9d811a5e21755096fa41503ff0e54c
Andriel <andriel.h@aluno.ifsp.edu.br>
2024-10-31 22:15:32 -0300

    POO c# Aula 01

---
+++ 31-10/ConsoleApp1/HelloWorld.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ConsoleApp1
+{
+    internal class HelloWorld
+    {
+        private string nome;
+
+        public HelloWorld()
+        {
+            nome = "Desconhecido";
+        }
+
+        public HelloWorld(string n)
+        {
+            nome = n;
+        }
+
+        public void sayHello()
+        {
+            Console.Write("Olá " + nome);
+        }
+    }
+}
---
+++ 31-10/ConsoleApp1/Porta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    internal class Porta
+    {
+        string cor = " ";
+        string origem = " ";
+        Boolean fechado = false;
+        string madeira = " ";
+        int tamanho = 0;
+
+        public Porta(string cor, string origem, bool fechado, string madeira, int tamanho)
+        {
+            this.cor = cor;
+            this.origem = origem;
+            this.fechado = fechado;
+            this.madeira = madeira;
+            this.tamanho = tamanho;
+        }
+
+        public string getCor{ get { return cor; } }
+        public string getrigem{ get { return origem; } }  
+        public Boolean getFechado{ get {   return fechado; } }
+        public int getTamanho{ get { return tamanho; } }
+
+        
+
+    }
+}
